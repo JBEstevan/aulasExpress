da50cbc29e624bc4c1df962db73d349acfd4c916
JBEstevan <jbe.estevan@gmail.com>
2024-08-19 19:42:43 -0300

    exercicio-baralho

---
+++ baralho/index.js
@@ -0,0 +1,11 @@
+const express = require("express")();
+const app = express();
+const port = 3001;
+
+app.get("/naipe", (req, res) => {
+
+})
+
+app.listen(port, () => {
+    console.log("isso executa")
+})
