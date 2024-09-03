447663a3cff898c739759c0ac4e9e02dcb09098c
JBEstevan <jbe.estevan@gmail.com>
2024-09-02 21:13:28 -0300

    feat: atualizando

--- baralho/index.js
+++ baralho/index.js
@@ -1,11 +1,35 @@
-const express = require("express")();
+const naipe_controller = require("./controllers/naipe.js")
+const express = require("express");
 const app = express();
 const port = 3001;
 
-app.get("/naipe", (req, res) => {
+app.use(express.json())
 
+app.get("/naipe", (req, res) => {
+    const content = naipe_controller.index()
+    res.json(content)
+})
+
+app.get("/naipe/:id", (req, res) => {
+    const content = naipe_controller.show(req.params.id)
+    res.json(content)
+})
+
+app.post("/naipe", (req, res) => {
+    const code = naipe_controller.store(req.body)
+    res.status(code).json()
+})
+
+app.put("/naipe/:id", (req, res) => {
+    const code = naipe_controller.update(req.params.id, req.body)
+    res.status(code).json()
+})
+
+app.delete("/naipe/:id", (req, res) => {
+    const code = naipe_controller.destroy(req.params.id);
+    res.status(code).json()
 })
 
 app.listen(port, () => {
-    console.log("isso executa")
+    console.log("isso executa quando o servidor inicia")
 })
